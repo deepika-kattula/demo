0477d7e39dea349e378a156a8262bc246e433b9e
deepika-kattula <134988812+deepika-kattula@users.noreply.github.com>
2026-03-16 16:43:23 +0530

    Rename Demo1Application to Demo1Application1
    Renamed the Demo1Application class to Demo1Application1.

--- demo1/demo1/src/main/java/com/example/demo1/Demo1Application.java
+++ demo1/demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application {
+public class Demo1Application1 {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
