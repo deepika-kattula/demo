f080f3679f9aacfdf91b78cf65dea34f444c1ac3
Deepika <kattuladeepika1234@gmai.com>
2026-03-16 16:44:59 +0530

    added new feature

--- demo1/demo1/src/main/java/com/example/demo1/Demo1Application.java
+++ demo1/demo1/src/main/java/com/example/demo1/Demo1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application1 {
+public class Demo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
